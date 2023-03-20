12916140434cc4d0b74072a2ea0863d7c1f34e75
onlystarczyw <onlystarczy@163.com>
2023-03-20 21:04:25 +0800

    re

---
+++ sql/mng.sql
@@ -0,0 +1,198 @@
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for tb_address
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_address`;
+CREATE TABLE `tb_address`  (
+  `id` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'uuid',
+  `introduction` longtext CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '介绍',
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '名称',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '摄影地址' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tb_address
+-- ----------------------------
+INSERT INTO `tb_address` VALUES ('1', 'ads', 'dsadsa');
+
+-- ----------------------------
+-- Table structure for tb_collect
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_collect`;
+CREATE TABLE `tb_collect`  (
+  `user_id` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户id',
+  `biz_id` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '套餐id、摄影作品id',
+  `type` int(11) NULL DEFAULT NULL COMMENT '1套餐，2摄影作品'
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '收藏表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tb_collect
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for tb_comment
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_comment`;
+CREATE TABLE `tb_comment`  (
+  `id` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'uuid',
+  `content` varchar(1000) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '评论',
+  `prefer_id` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '套餐id',
+  `user_id` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户id',
+  `parent_id` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '回复的评论id',
+  `order_id` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '订单id',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '评价评论表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tb_comment
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for tb_order
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_order`;
+CREATE TABLE `tb_order`  (
+  `id` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'uuid',
+  `create_time` datetime NULL DEFAULT NULL COMMENT '下单时间',
+  `state` int(11) NULL DEFAULT NULL COMMENT '1未支付，2支付',
+  `pay_time` datetime NULL DEFAULT NULL COMMENT '支付时间',
+  `prefer_id` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '套餐id',
+  `user_id` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户id',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '订单' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tb_order
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for tb_preferential
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_preferential`;
+CREATE TABLE `tb_preferential`  (
+  `id` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'uuid',
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '名称',
+  `imgurl` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '照片路径',
+  `introduction` longtext CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '说明',
+  `price` decimal(11, 2) NULL DEFAULT NULL COMMENT '价格',
+  `adress` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '地址',
+  `type` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '类型',
+  `create_time` datetime NULL DEFAULT NULL COMMENT '发布时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '摄影套餐' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tb_preferential
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for tb_reserve
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_reserve`;
+CREATE TABLE `tb_reserve`  (
+  `id` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `start_time` datetime NULL DEFAULT NULL COMMENT '预约开始时间',
+  `end_time` datetime NULL DEFAULT NULL COMMENT '预约结束时间',
+  `state` int(11) NULL DEFAULT NULL COMMENT '预约状态',
+  `worker_id` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '工作人员id',
+  `order_id` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '订单id',
+  `user_id` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '客户id',
+  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '预约表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tb_reserve
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for tb_type
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_type`;
+CREATE TABLE `tb_type`  (
+  `id` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'uuid',
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '类型名称',
+  `user_id` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户id',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '摄影风格类型' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tb_type
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for tb_user
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_user`;
+CREATE TABLE `tb_user`  (
+  `id` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'uuid',
+  `password` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '密码',
+  `state` tinyint(255) NULL DEFAULT NULL COMMENT '0商家，1客户',
+  `username` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户名',
+  `birthday` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '生日',
+  `eamil` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '邮箱',
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '姓名',
+  `phone` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '手机号',
+  `sex` int(11) NULL DEFAULT NULL COMMENT '性别',
+  `user_id` int(11) NULL DEFAULT NULL COMMENT '用户id',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tb_user
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for tb_user_photo_rel
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_user_photo_rel`;
+CREATE TABLE `tb_user_photo_rel`  (
+  `user_id` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `photo_id` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`user_id`, `photo_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tb_user_photo_rel
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for tb_worker
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_worker`;
+CREATE TABLE `tb_worker`  (
+  `id` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'uuid',
+  `imgurl` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '头像',
+  `introduction` longtext CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '介绍',
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '姓名',
+  `phone` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '电话',
+  `sex` tinyint(2) NULL DEFAULT NULL COMMENT '性别',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '工作人员（摄影师）' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tb_worker
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for tb_worker_demo
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_worker_demo`;
+CREATE TABLE `tb_worker_demo`  (
+  `id` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'uuid',
+  `imgurl` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '作品图片url',
+  `desc` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '备注',
+  `type` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '作品类型',
+  `worker_id` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '作者id',
+  `create_time` datetime NULL DEFAULT NULL COMMENT '创作时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '摄影师作品' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tb_worker_demo
+-- ----------------------------
+
+SET FOREIGN_KEY_CHECKS = 1;
